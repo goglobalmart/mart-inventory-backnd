6b93f82ddf3b2348e66c3db35d1f1e38bf4b71a6
goglobalschooldev <goglobalschool15@gmail.com>
2022-08-26 15:46:05 +0700

    add pupolate release product

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const typeDefs = mergeTypeDefs(typeDefsFiles)
 
 const startServer = async () => {
     await connect(`${process.env.DB_PRODUCTION_PORT}`)
-    // await connect(`${process.env.DB_DEV_PORT}`)
+    // await connect(`${process.env.DB_DEV_PORT}`)v
         .then(e => console.log('DB Connected'))
         .catch(er => console.log('DB Connection Error', er));
     const app = express()
