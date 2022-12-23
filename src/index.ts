74bb58146261b61c3547350228bc9c74121f8ccd
Rathnana <goglobalschool15@gmail.com>
2022-12-23 18:49:49 +0700

    adjust stock

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ const resolvers = mergeResolvers(resolversFiles)
 const typeDefs = mergeTypeDefs(typeDefsFiles)
 
 const startServer = async () => {
-    // await connect(`${process.env.DB_PRODUCTION_PORT}`)
-    await connect(`${process.env.DB_DEV_PORT}`)
+    await connect(`${process.env.DB_PRODUCTION_PORT}`)
+    // await connect(`${process.env.DB_DEV_PORT}`)
         .then(e => console.log('DB Connected'))
         .catch(er => console.log('DB Connection Error', er));
     const app = express()
